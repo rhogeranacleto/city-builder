54bd1abd54e0edba4b635b9ab72622e7770b5372
rhogeranacleto <rhogeranacleto@gmail.com>
2025-08-30 18:10:44 +0200

    adding state

--- person.gd
+++ person.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
-var SPEED:= 100.0
-var state: String = 'idle'
+var SPEED:= 200.0
+var state: String = 'idle' :
+	set = set_state
 
 @export var target: Node2D
-@export var workPlace: Node2D
+@export var work_place: Node2D
 
 @onready var action: Label = $Action
 @onready var chop_timer: Timer = $ChopTimer
@@ -21,12 +22,29 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if state == 'walking':
-		if target:
-			walk_direction_target(delta)
-		else:
-			walk_back_to_work_place(delta)
+	if state == 'get_resource':
+		walk_direction_target(delta)
+	
+	if state == 'drop_resource':
+		walk_back_to_work_place(delta)
 
+	pass
+
+func set_state(new_state: String):
+	state = new_state
+	
+	match new_state:
+		'idle':
+			action.text = "procurando madeira"
+			lookForTree()
+		'chopping':
+			action.text = "Cortando"
+			chop_timer.start()
+		'drop_resource':
+			chop_timer.stop()
+			action.text = "Levando madeirada"
+			target = null
+	
 	pass
 
 func walk_direction_target(delta: float):
@@ -34,33 +52,31 @@ func walk_direction_target(delta: float):
 		global_position += position.direction_to(target.global_position) * SPEED * delta
 	else:
 		state = 'chopping'
-		chop_timer.start()
+		
 	pass
 
 func walk_back_to_work_place(delta: float):
-	if position.distance_to(workPlace.global_position) > 10:
-		global_position += position.direction_to(workPlace.global_position) * SPEED * delta
+	if position.distance_to(work_place.global_position) > 10:
+		global_position += position.direction_to(work_place.global_position) * SPEED * delta
+	else:
+		state = 'idle'
+		work_place.addResource(3)
 
 func chop() -> void:
-	action.text = "Cortando"
+	var treeIsDown = target.chop()
 	
-	var finished = target.chop()
-	
-	if finished:
-		print('cabo')
-		chop_timer.stop()
-		action.text = "Levando madeirada"
-		target = null
-		state = 'walking'
-	
+	if treeIsDown:
+		state = 'drop_resource'
+
 	pass
 
 func lookForTree() -> void:
-	var firstTree = get_tree().get_nodes_in_group('tree').get(0)
+	var trees = get_tree().get_nodes_in_group('tree')
 	
-	target = firstTree
-	
-	state = 'walking'
+	if not trees.is_empty():
+		target = trees.get(0)
+		
+		state = 'get_resource'
 	pass
 
 
--- storage.gd
+++ storage.gd
@@ -1,14 +1,29 @@
 extends Node2D
 
+var total := 0 :
+	set(value) :
+		total = value
+		
+		storage_name.text = "Storage " + type +  " " + str(total) + " / " + str(limit)
+		pass
+
+var limit := 100
+
 @export var type : String
 @onready var storage_name: Label = $StorageName
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	storage_name.text = "Storage " + type
+	storage_name.text = "Storage " + type + "0 / " + str(limit)
 	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+
+func addResource(amount: int) -> void:
+	total += amount
+	
 	pass
